4eb0f1c4ebd26610df3e20dec6789c535b275b23
LucyKiszczak <lucykiszczakphd@gmail.com>
2023-05-19 18:40:58 -0700

    Added files via upload

---
+++ Welcome_message_V2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Welcome_Accounting_exercise
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Welcome to AcmeX Accounting Systems! \nRemember, we’re “accounting” on you!");
+            Console.ReadLine();
+        }
+    }
+}
