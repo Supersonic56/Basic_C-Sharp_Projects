928d56f8b7ba05f8b2be88dd43f9871b13266219
Supersonic56 <lucykiszczakphd@gmail.com>
2023-05-23 15:26:09 -0700

    asked for a user input and conducted basic math operation using +-*/% operators

---
+++ Basic_math_Csharp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Basic_math_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Takes an input from the user, multiplies it by 50, then prints the result to the console. 
+            /*
+            int Number = Convert.ToInt32(Console.ReadLine());
+            int OtherNumber = 50;
+            int Combined = Number * OtherNumber;
+            Console.WriteLine(Combined);
+            Console.ReadLine();
+            */
+
+            //Takes an input from the user, adds 25 to it, then prints the result to the console.
+            /*
+            Console.WriteLine("Write any number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            int OtherNumber = 25;
+            int Combined = Number + OtherNumber;
+            Console.WriteLine(Combined);
+            Console.ReadLine();
+            */
+
+            //Takes an input from the user, divides it by 12.5, then prints the result to the console.
+            /*
+             Console.WriteLine("Write any number");
+             int Number = Convert.ToInt32(Console.ReadLine());
+             float OtherNumber = 12.5f;
+             float Combined = Number / OtherNumber;
+             Console.WriteLine(Combined);
+             Console.ReadLine();
+            */
+
+            //Takes an input from the user, checks if it is greater than 50, then prints the true/false result to the console.
+            /*
+            Console.WriteLine("Write any number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            int OtherNumber = 50;
+            bool Greater = Number > OtherNumber;
+            Console.WriteLine(Greater);
+            Console.ReadLine();
+            */
+
+            //Takes an input from the user, divides it by 7, then prints the remainder to the console (tip: think % operator).
+            /*
+            Console.WriteLine("Write any number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            int OtherNumber = 7;
+            int Combined = Number % OtherNumber;
+            Console.WriteLine(Combined);
+            Console.ReadLine();
+            */
+
+
+            Console.WriteLine("Write any number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            int OtherNumber = 7;
+            int Combined = Number % OtherNumber;
+            Console.WriteLine(Combined);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
+
+
