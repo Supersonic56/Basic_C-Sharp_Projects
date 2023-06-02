c28df0b2c9137d1b249978393426bec4de8fc2ed
Supersonic56 <lucykiszczakphd@gmail.com>
2023-06-01 18:55:52 -0700

    Step 1 in creating the black jack game. a program has been created with two classes to count all the cards in a deck of card

---
+++ black_jack_21_game/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace black_jack_21_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            /*           
+            deck.Cards = new List(Card);     
+            Card cardOne = new Card();
+            cardOne.Face = "Queen";
+            cardOne.Suit = "Spades";
+            */
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
+        }
+    }
+}
