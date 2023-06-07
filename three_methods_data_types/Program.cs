411645b567d949d5850544051a072794b8048c95
Supersonic56 <lucykiszczakphd@gmail.com>
2023-06-06 17:45:27 -0700

    a drill where a method called displays the result. the method takes different data types as  parameters

---
+++ three_methods_data_types/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using three_methods_data_types_Class1;
+
+namespace three_methods_data_types
+{
+    public class MathOperations
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the class
+            MathOperations2 mathOps = new MathOperations2();
+
+            // Call the first method and display the result
+            int result1 = mathOps.DoMath(3);
+            Console.WriteLine(result1);
+            Console.ReadLine();
+
+            // Call the second method and display the result
+            int result2 = mathOps.DoMath(3.5m);
+            Console.WriteLine(result2);
+            Console.ReadLine();
+
+            // Call the third method and display the result
+            int result3 = mathOps.DoMath("4");
+            Console.WriteLine(result3);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ three_methods_data_types/three_methods_data_types_Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace three_methods_data_types_Class1
+{
+    public class MathOperations2
+    {
+        // Method that takes in an integer and returns the result of a math operation
+        public int DoMath(int num)
+        {
+            return num + 5;
+        }
+
+        // Method that takes in a decimal and returns the result of a math operation
+        public int DoMath(decimal num)
+        {
+            return (int)(num * 2);
+        }
+
+        // Method that takes in a string, converts it to an integer if possible,
+        // and returns the result of a math operation
+        public int DoMath(string str)
+        {
+            int num;
+            if (int.TryParse(str, out num))
+            {
+                return num - 3;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
