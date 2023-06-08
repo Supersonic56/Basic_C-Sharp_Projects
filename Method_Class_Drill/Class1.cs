cdd9a58b725dafb3c6e66fcd59564e945b08cea7
Supersonic56 <lucykiszczakphd@gmail.com>
2023-06-07 18:23:09 -0700

    created a console app with a class containing a void method that takes two integers as parameters. the method performs an operation on the first integer and siplays second integer

---
+++ Method_Class_Drill/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Method_Class_Drill
+{
+    public class MyClass
+    {
+        // This method takes two integers as parameters
+        // It performs a math operation on the first integer and displays the second integer to the screen
+        public void MyMethod(int num1, int num2)
+        {
+            int result = num1 * 2; // Multiply the first integer by a given number
+            Console.WriteLine(num2); // Display to the screen the second integer which is a product of multiplication the first integer by a number
+        }
+    }
+}
---
+++ Method_Class_Drill/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Method_Class_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass(); // Instantiate the class
+
+            myClass.MyMethod(5, 10); // Call the method in the class, passing in two numbers
+
+            myClass.MyMethod(num1: 3, num2: 6); // Call the method in the class, specifying the parameters by name
+        }
+    }
+}
