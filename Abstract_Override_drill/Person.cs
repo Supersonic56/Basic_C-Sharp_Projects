d33a47fc96d2b5301189f8a5c96178720a98fda7
Supersonic56 <lucykiszczakphd@gmail.com>
2023-06-12 17:53:39 -0700

    updated the abstract override methods

--- Abstract_Override_drill/Person.cs
+++ Abstract_Override_drill/Person.cs
@@ -6,23 +6,21 @@ namespace Abstract_Override_drill
     public abstract class Person
     {
         // Properties for first and last name
-        public string firstName { get; set; }
-        public string lastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
         // Abstract method SayName
         public abstract void SayName();
     }
-
-    // Class Employee that inherits from Person
-    public class Employee : Person
-    {
-        // Implementation of SayName method
-        public override void SayName()
-        {
-            Console.WriteLine("Name: " + firstName + " " + lastName);
-        }
-    }
 }
+/*
+Create an abstract class called Person with two properties: string firstName and string lastName.
+Give it the method SayName().
+Create another class called Employee and have it inherit from the Person class.
+Implement the SayName() method inside of the Employee class.
+Inside the Main() method, instantiate an Employee object with firstName “Sample” and lastName “Student”. Call the SayName() method on the object.
+
+*/
 
 
 
--- Abstract_Override_drill/Program.cs
+++ Abstract_Override_drill/Program.cs
@@ -2,18 +2,25 @@
 
 namespace Abstract_Override_drill
 {
-    class PersonX
+    class Program
     {
         static void Main(string[] args)
         {
             // Instantiate an Employee object with firstName "Sample" and lastName "Student"
-            Employee employee = new Employee { firstName = "Sample", lastName = "Student" };
-
-            // Call the SayName method on the object
+            Employee employee = new Employee { FirstName = "Sample", LastName = "Student" };
+            //Call the superclass method SayName() on the Employee object
             employee.SayName();
-
             Console.ReadLine();
         }
-    }
+    } 
 }
 
+
+/*
+Create an abstract class called Person with two properties: string firstName and string lastName.
+Give it the method SayName().
+Create another class called Employee and have it inherit from the Person class.
+Implement the SayName() method inside of the Employee class.
+Inside the Main() method, instantiate an Employee object with firstName “Sample” and lastName “Student”. Call the SayName() method on the object.
+
+*/
