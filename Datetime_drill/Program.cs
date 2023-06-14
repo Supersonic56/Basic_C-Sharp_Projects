2a8d0bd9f0c56970802ede83e9b767b67a5c1347
Supersonic56 <lucykiszczakphd@gmail.com>
2023-06-14 15:18:21 -0700

    datetime method drill

---
+++ Datetime_drill/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Datetime_drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print the current date and time to the console
+            DateTime now = DateTime.Now;
+            Console.WriteLine($"The current date and time is: {now}");
+
+            // Ask the user for a number
+            Console.Write("Please enter a number of hours: ");
+            int hours = int.Parse(Console.ReadLine());
+
+            // Calculate the time it will be in X hours
+            DateTime futureTime = now.AddHours(hours);
+
+            // Print the future time to the console
+            Console.WriteLine($"In {hours} hours, it will be: {futureTime}");
+        }
+    }
+}
+
+
+
