6bd2bb713b62c29490d7937ac1ff5b419c381e0b
Supersonic56 <lucykiszczakphd@gmail.com>
2023-06-15 11:30:11 -0700

    A Rectangle class with three chaining constructors. Added a GetArea method to the Rectangle class that calculates the area of the rectangle by multiplying its width and height

---
+++ Constructor_Chaining_drill/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Chaining_drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new Rectangle object using the first constructor
+            Rectangle rect1 = new Rectangle();
+
+            // Create a new Rectangle object using the second constructor
+            Rectangle rect2 = new Rectangle(10, 20);
+
+            // Create a new Rectangle object using the third constructor
+            Rectangle rect3 = new Rectangle(30);
+
+            // Use the GetArea method to calculate the area of each rectangle
+            int area1 = rect1.GetArea();
+            int area2 = rect2.GetArea();
+            int area3 = rect3.GetArea();
+
+            // Print the areas of the rectangles
+            Console.WriteLine("Area of rect1: " + area1);
+            Console.WriteLine("Area of rect2: " + area2);
+            Console.WriteLine("Area of rect3: " + area3);
+        }
+    }
+}
---
+++ Constructor_Chaining_drill/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Chaining_drill
+{
+    class Rectangle
+    {
+        private int width;
+        private int height;
+
+         // This is the first constructor.
+         // It takes no arguments and sets default values for width and height.
+        public Rectangle() : this(0,0)
+        {
+            // This constructor is empty because all the work is done by the second constructor.
+        }
+        // This is the second constructor. It takes two arguments and sets the values for width and height.
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        // This is the third constructor. It takes one argument and sets the value for width. The value for height is set to a default value by calling the second constructor.
+        public Rectangle(int width) : this(width, 0)
+        {
+            // This constructor is empty because all the work is done by the second constructor.
+        }
+
+        // Method to calculate the area of the rectangle
+        public int GetArea()
+        {
+            return width * height;
+        }
+    }
+}
