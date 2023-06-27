c711d544fcfcc360a22a275b7217d9c56a21f90e
Lucyna Kiszczak <lucykiszczakphd@gmail.com>
2023-06-27 09:37:31 -0700

    created a code-first application using entity framework 6.x .NET 4.x. Created and established a database connection using solution manager/properties/settings

---
+++ studentDB/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace studentDB
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SchoolContext())
+            {
+                // Create and save a new Student
+                Console.Write("Enter last name for a new Student: ");
+                var name = Console.ReadLine();
+                Console.Write("Enter first name for a new Student: ");
+                var firstname = Console.ReadLine();
+
+                var student = new Student { LastName = name, FirstName = firstname };
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                // Display all Students from the database
+                var query = from s in db.Students
+                            orderby s.LastName
+                            select s;
+
+                Console.WriteLine("All students in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine("Name: " + item.LastName + ", " + item.FirstName);
+                }
+
+               
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
